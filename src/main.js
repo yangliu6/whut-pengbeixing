2eac14146e772e1670f7f590c0fe38bd0870618e
Mr.Hou <hsk6543210@foxmail.com>
2018-05-09 10:07:58 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import App from './App'
+import axios from 'axios'
+import store from './store'
+import '@/styles/globalTheme.scss'
+// import '@/assets/icons/iconfont'
+
+Vue.use(axios)
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+new Vue({
+    el: '#app',
+    router,
+    store,
+    template: '<App/>',
+    components: { App }
+})
